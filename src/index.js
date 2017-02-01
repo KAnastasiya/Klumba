c93cebf2425d79081134d6122982a93e0b862543
Anastasiia <igoninan87@gmail.com>
2017-02-01 10:48:40 +0200

    Replace browserHistory on hashHistory

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <ReactRedux.Provider store={store}>
-    <ReactRouter.Router history={ReactRouter.browserHistory} routes={routes} />
+    <ReactRouter.Router history={ReactRouter.hashHistory} routes={routes} />
   </ReactRedux.Provider>,
   document.querySelector('.app'),
 );
